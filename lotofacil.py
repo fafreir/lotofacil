52f2965c93767cfd0f3cfb785c599ff244883957
fafreir <63565855+fafreir@users.noreply.github.com>
2023-10-18 06:04:49 -0300

    Update lotofacil.py

--- lotofacil.py
+++ lotofacil.py
@@ -1,33 +1,16 @@
-import random
-import os
+# Gerador Lotofácil
+from random import randint
 
-# Gerando os 15 numeros
-lista = list()
-for i in range(1, 16):
-    sorteio = random.choices(range(1, 26))
-    if sorteio not in lista:
-        lista.append(sorteio)
+x = 0
+lista = []
+
+while x <= 14:
+    sorteado = randint(1, 25)
+    if sorteado not in lista:
+        lista.append(sorteado)
     else:
-        while sorteio in lista:
-            sorteio = random.choices(range(1, 26))
-        else:
-            lista.append(sorteio)
+        x = x - 1
+    x += 1
 
-
-def formatacao(lista):
-    return str(lista).replace('[', '').replace(']', '')
-
-
-# Conversão para inteiro
-try:
-    number = list()
-    for i in range(0, 16):
-        word = str(sorted(lista[i])).replace('[', '').replace(']', '')
-        a = int(word)
-        number.append(a)
-except IndexError:
-    pass
-
-ordenacao = formatacao(sorted(number))
-
-print(f'Os numeros sorteados são: {os.linesep} {(ordenacao)}')
+lista.sort()
+print(f'Gerador Lotofácil: {str(lista)[1:-1]}')
